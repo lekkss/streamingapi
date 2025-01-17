41ea3c05573e5fb57918f55378de39855066df44
lekkss <leksguy97@gmail.com>
2025-01-17 22:28:50 +0000

    feat: videos api - stream video by id

--- src/main/java/com/lekkss/streamingapi/controllers/VideoController.java
+++ src/main/java/com/lekkss/streamingapi/controllers/VideoController.java
@@ -3,6 +3,9 @@ package com.lekkss.streamingapi.controllers;
 import com.lekkss.streamingapi.models.Video;
 import com.lekkss.streamingapi.services.VideoService;
 import com.lekkss.streamingapi.utils.Response;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +24,20 @@ public class VideoController {
         Response<?> videos = videoService.getAllVideos();
         return ResponseEntity.ok(videos);
     }
+
+    @GetMapping("/stream/{videoId}")
+    public ResponseEntity<Resource> stream(@PathVariable Integer videoId) {
+
+        Video video = videoService.getVideo(videoId);
+        String contentType = video.getContentType();
+        String filePath = video.getFilePath();
+        Resource resource = new FileSystemResource(filePath);
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
+    }
+
 
     @PostMapping
     public ResponseEntity<Response<?>> createVideo(
